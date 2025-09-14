e5fe8b9e397fd32ef03e1c3838831072b65b0d89
bobbepallisri <srikanthbobbepalli3@gmail.com>
2025-09-14 21:59:15 +0530

    Node.js practice repo with Day6 folders

---
+++ DAY6/myapp/index.js
@@ -0,0 +1,72 @@
+const express=require('express');
+const app=express();
+app.use(express.json());
+const sqlite3=require('sqlite3');
+const sqlite=require('sqlite')
+const path=require('path');
+const dbPath=path.join(__dirname,'newdatabase.db');
+const {open}=require('sqlite');
+const bcrypt = require('bcrypt');
+
+let db=null;
+const initlizeDBAndServer=async()=>{
+    try{db=await open({
+        filename:dbPath,
+        driver:sqlite3.Database
+    })
+    app.listen(4000,()=>{
+    console.log('server is running in the port 4000')})
+
+}catch(e){
+
+        console.log(`error message ${e.message}`);
+    }
+}
+
+initlizeDBAndServer();
+
+app.post("/users/", async (request, response) => {
+  const { username, name, password, gender, location } = request.body;
+  const hashedPassword = await bcrypt.hash(request.body.password, 10);
+  const selectUserQuery = `SELECT * FROM users WHERE username = '${username}'`;
+  const dbUser = await db.get(selectUserQuery);
+  if (dbUser === undefined) {
+    const createUserQuery = `
+      INSERT INTO 
+        users (username, name, password, gender, location) 
+      VALUES 
+        (
+          '${username}', 
+          '${name}',
+          '${hashedPassword}', 
+          '${gender}',
+          '${location}'
+        )`;
+    const dbResponse = await db.run(createUserQuery);
+    const newUserId = dbResponse.lastID;
+    response.send(`Created new user with ${newUserId}`);
+  } else {
+    response.status = 400;
+    response.send("User already exists");
+  }
+});
+
+app.post("/login", async (request, response) => {
+  const { username, password } = request.body;
+  const selectUserQuery = `SELECT * FROM users WHERE username = '${username}'`;
+  const dbUser = await db.get(selectUserQuery);
+  if (dbUser === undefined) {
+    response.status(400);
+    response.send("Invalid User");
+  } else {
+    const isPasswordMatched = await bcrypt.compare(password, dbUser.password);
+    if (isPasswordMatched === true) {
+      response.send("Login Success!");
+    } else {
+      response.status(400);
+      response.send("Invalid Password");
+    }
+  }
+});
+
+
